48673062d05a7b4cbb48f384f5533989cd4a96fa
Tago-F <snake112381@gmail.com>
2025-09-23 18:44:11 +0900

    JpaSpecificationExecutor を実装

--- backend/src/main/java/com/tagoapp/backend/repository/EquipmentRepository.java
+++ backend/src/main/java/com/tagoapp/backend/repository/EquipmentRepository.java
@@ -1,10 +1,11 @@
 package com.tagoapp.backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import com.tagoapp.backend.entity.Equipment;
 
 @Repository
-public interface EquipmentRepository extends JpaRepository<Equipment, Long> {
+public interface EquipmentRepository extends JpaRepository<Equipment, Long>, JpaSpecificationExecutor<Equipment> {
 }
